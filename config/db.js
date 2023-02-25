9124a2adafa1d464159c6aace8833df22ab19c78
rubij8 <jimenezr115@gmail.com>
2023-02-24 23:38:45 -0600

    Configuración iinicial del Servidor y la Base de Datos 2

---
+++ config/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+//Se extrae la propiedas sequielize 
+const { Sequelize } = require('sequelize');
+
+ /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, { 
+    host: process.env.DB.HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_ENGINE
+  });
+
+
