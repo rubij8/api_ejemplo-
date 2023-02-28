3447f0486484934332f5cc319278928827e45d13
rubij8 <jimenezr115@gmail.com>
2023-02-28 00:19:12 -0600

    Conexion dinamica a BD

--- config/db.js
+++ config/db.js
@@ -4,11 +4,19 @@ const { Sequelize } = require('sequelize');
 
  /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 
+const {DB_NAME,
+       DB_USER,
+       DB_PASS,
+       DB_HOST, 
+       DB_PORT, 
+       DB_ENGINE
+} = process.env;
+
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, { 
-    host: process.env.DB.HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_ENGINE
-  });
+});
 
   
 
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // Hacer accesible variables de entorno declaradas en el archivo ".env"
 require('dotenv').config();
 
-
 const express = require('express');//Importar libreria, paquete o dependencia 
 const app = express();//Ejecutar libreria para crear servidor 
-const port = +process.env.API_PORT;
+const db = require ('./config/db');
+
+const{API_PORT, DB_ENGINE} = process.env;
+
+db.sync().then(() => {
+   console.log('BD Conectada .. :)');
+   console.log(`SGBD: ${DB_ENGINE.toUpperCase() }`);
+})
+.catch((error) => {
+   console.log(`Se ah encontado el siguiente error: :(${error}`);
+});
+
+
+//const port = +process.env.API_PORT;
+
   //signo de mas (+) indica que se convierten a numericos
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -14,6 +27,6 @@ app.get('/saludo', (req, res) => {
     res.send('Hola Mario.... Ejemplo de ruta' );
 });
 
-app.listen(port, () => {
-  console.log(`Servido escuchando en el puerto: ${port}`);
+app.listen(+API_PORT, () => {
+  console.log(`Servido escuchando en el puerto: ${API_PORT}`);
 });
